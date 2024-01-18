8ce114bc7c4f43c7e9bf56b8cfb032a93717478b
Vahid Spacer <108230053+Vahid-Spacer@users.noreply.github.com>
2024-01-18 19:58:05 +0100

    Update icmp.sh

--- icmp.sh
+++ icmp.sh
@@ -7,8 +7,6 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 distro=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
-thisServerIP=$(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
-networkInterfaceName=$(ip -o -4 route show to default | awk '{print $5}')
 
 if [[ $distro != "ubuntu" ]]; then
 	echo "distro not supported please use ubuntu"
@@ -24,25 +22,15 @@ echo "+--------------------------------------------------+"
 echo " Select one of the following options"
 echo "  1.  Server tunnel (ipv4)"
 echo "  2.  Remove the tunnel"
-echo "  3.  View the Forwarded IP (ipv4)"
-echo "  4.  Server tunnel (ipv6)"
-echo "  5.  View the Forwarded IP (ipv6)"
-echo "  6.  Exit"
-read -r -p "Please select one [1-2-3]: " -e OPTION
+echo "  3.  Exit"
+read -r -p "Please select one [1-2-3-4-5-6]: " -e OPTION
 case $OPTION in
 1)
-sudo iptables -t nat -F
-    echo "Your forward port was removed"
-  ;;
-2)
 sudo iptables -A INPUT -p icmp -j DROP
-echo "+--------------------------------------------------+"
-echo "|                        B Y                       |"
-echo "|                 D E V S P A C E X                |"
-echo "|           ----------------------------           |"
-echo "|                 <Ping is closed>                 |"
-echo "+--------------------------------------------------+"
-exit
+    echo "Saving iptables rules..."
+  ;;
+  2)
+iptables -t nat -L --line-numbers
   ;;
     3)
     exit
